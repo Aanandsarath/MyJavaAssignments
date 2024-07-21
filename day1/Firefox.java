a1d367d566f8ddade66e0b2e4d2104ec33656e8f
Aanandsarath <59552185+Aanandsarath@users.noreply.github.com>
2024-07-21 14:13:50 +0530

    Add files via upload
    Week1 Day1 of Core Java

---
+++ day1/Firefox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String browserName = "Firefox";
+		Boolean isVisible = true;
+		int releaseyear = 1998;
+		char browserlogo = 'f';
+		
+		System.out.println("The Browser Version is "+browserVersion);
+		System.out.println("The Browser Name is "+browserName);
+		System.out.println("The Browser is visible ? "+isVisible);
+		System.out.println("The Release year of the browser is "+releaseyear);
+		System.out.println("The Browser Logo is "+browserlogo);
+		
+
+	}
+
+}
---
+++ day1/FirstJava.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class FirstJava {
+	//Main -> ctrl + space
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Testleaf");
+		System.out.println("Welcome to My first java program");
+		System.out.println(10+10);
+	}
+	
+
+}
