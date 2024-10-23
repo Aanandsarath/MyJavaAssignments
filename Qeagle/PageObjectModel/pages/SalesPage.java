60aa46ba67f5c8f57bfce2990ec5a176c927c6dd
Aanandsarath <59552185+Aanandsarath@users.noreply.github.com>
2024-10-23 16:12:06 +0530

    Add files via upload

---
+++ Qeagle/PageObjectModel/pages/SalesPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import baseclass.ProjectSpecificMethod;
+
+public class SalesPage extends ProjectSpecificMethod {
+	
+	public SalesPage(ChromeDriver driver) {
+		this.driver=driver;
+	}
+	
+	
+	public CreateOpportunityPage clickOnOppTab() {
+		WebElement opp = driver.findElement(By.xpath("//span[text()='Opportunities']"));
+		JavascriptExecutor executor = (JavascriptExecutor)driver;
+		executor.executeScript("arguments[0].click();", opp);
+		return new CreateOpportunityPage(driver);
+	}
+
+}
---
+++ Qeagle/PageObjectModel/runner/CreateOpportunity_TC001.java
@@ -0,0 +1,29 @@
+package runner;
+
+import org.testng.annotations.Test;
+
+import baseclass.ProjectSpecificMethod;
+import pages.LoginPage;
+
+public class CreateOpportunity_TC001 extends ProjectSpecificMethod{
+	
+	@Test
+	public void runner() {
+		LoginPage lp = new LoginPage(driver);
+		lp.enterUsername().
+		enterPassword().
+		clickOnLogin().
+		clickOnWaffleButton().
+		clickonViewAll().
+		clickOnSales().
+		clickOnOppTab().
+		clickonnew().
+		entername().
+		enteramount().
+		selectdate().
+		selectstage().
+		clickonSave().
+		verifyOpp();
+	}
+
+}
